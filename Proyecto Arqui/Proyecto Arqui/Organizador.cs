49a580f315bd13e0fac7d27a5979bb4cee5d6f7a
jeffryvenegas <jeffvene@gmail.com>
2016-10-16 16:51:50 -0600

    Cambios en cantidad de signal. Control quantum ejecutarInstr

--- Proyecto Arqui/Proyecto Arqui/Organizador.cs
+++ Proyecto Arqui/Proyecto Arqui/Organizador.cs
@@ -4,8 +4,8 @@ namespace Proyecto_Arqui
 {
     public class Organizador
     {
-        int _cantHilillos;
-        int _quantum;
+        int _CANTHILILLOS;
+        int _QUANTUM;
         int reloj;
         Procesador principal;
         Procesador procesador1;
@@ -15,8 +15,8 @@ namespace Proyecto_Arqui
 
         public Organizador(int cantHilillos, int quantum, string path)
         {
-            _cantHilillos = cantHilillos;
-            _quantum = quantum;
+            _CANTHILILLOS = cantHilillos;
+            _QUANTUM = quantum;
             sincronizacion = new Barrier(4); //barrera para los tres procesadores y procesador principal
 
             procesador1 = new Procesador(1, sincronizacion);
@@ -29,40 +29,34 @@ namespace Proyecto_Arqui
 
             cargarMemoria(path);
             inicializaProcesadores();
-           
-
         }
 
         private void repartirHilillos(Thread hilo_proc1, Thread hilo_proc2, Thread hilo_proc3)
         {
-          
-      
+
+
         }
 
         //el procesador principal carga en memoria las instrucciones contenidas en los txt
-        public void cargarMemoria(string path) {
+        public void cargarMemoria(string path)
+        {
             principal = new Procesador();
             principal.inicializarProcesadorPrincipal();
-            principal.Visible=true;
+            principal.Visible = true;
             principal.cargarInstrucciones(path);
         }
         //Crea los hilos que corresponden a cada uno de los nucleos o procesadores, en total se simulan 3 procesadores
         private void inicializaProcesadores()
         {
-            Thread hilo_proc1= new Thread(delegate(){  });
-
-            Thread hilo_proc2 = new Thread(delegate(){  });
-
-            Thread hilo_proc3 = new Thread(delegate(){  });
-           
+            Thread hilo_proc1 = new Thread(delegate () { });
+            Thread hilo_proc2 = new Thread(delegate () { });
+            Thread hilo_proc3 = new Thread(delegate () { });
 
             hilo_proc1.Start();
             hilo_proc2.Start();
             hilo_proc3.Start();
 
             repartirHilillos(hilo_proc1, hilo_proc2, hilo_proc3);
-
-           
         }
     }
 }
